8ef096ac087942c24f79b8aaeb6ef8c8960d0505
ckomodo <chrissakwa@gmail.com>
2020-10-16 21:45:48 -0700

    all files populated

--- db/schema.sql
+++ db/schema.sql
@@ -7,5 +7,6 @@ CREATE TABLE burgers
 (
 	id INT NOT NULL AUTO_INCREMENT,
 	name VARCHAR(255) NOT NULL,
+	devour BOOLEAN DEFAULT false,
 	PRIMARY KEY (id)
 );
---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (name) VALUES ('Cheese');
+INSERT INTO burgers (name) VALUES ('Bacon');
+INSERT INTO burgers (name, devour) VALUES ('Cheese-egg', true);
+INSERT INTO burgers (name, devour) VALUES ('Veggie', true);
+INSERT INTO burgers (name, devour) VALUES ('Vegan', true);
+INSERT INTO burgers (name) VALUES ('Ham');
